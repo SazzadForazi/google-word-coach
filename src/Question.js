2ef8734bf2caa92b89d309442739ae4d1a4b6df3
Sazzad Hossain <sazzad.csebubt@gmail.com>
2022-01-31 13:31:05 +0600

    first commit

--- src/Question.js
+++ src/Question.js
@@ -27,20 +27,16 @@ export default class Question extends Component {
                     choices: ["https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Flag_of_Bangladesh.svg/800px-Flag_of_Bangladesh.svg.png", "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/255px-Flag_of_India.svg.png"],
                     answer: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Flag_of_Bangladesh.svg/800px-Flag_of_Bangladesh.svg.png"
                 },
-                {
-                    question: " which flag in canada??",
-                    choices: ["https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/800px-Flag_of_Japan.svg.png", " https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/255px-Flag_of_Canada_%28Pantone%29.svg.png"],
-                    answer: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/255px-Flag_of_Canada_%28Pantone%29.svg.png"
-                },
+
                 {
                     question: "which flag in argintina?",
                     choices: ["https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/800px-Flag_of_Brazil.svg.png", "https://cdn.britannica.com/69/5869-004-7D75CD05/Flag-Argentina.jpg"],
                     answer: "https://cdn.britannica.com/69/5869-004-7D75CD05/Flag-Argentina.jpg"
                 },
                 {
-                    question: "which flag in Brazil?",
-                    choices: ["https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/800px-Flag_of_Brazil.svg.png", "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53E6/2F6A/5EBE/3F5A/0752/0A28/100B/04C1/Flag_of_Uruguay.png"],
-                    answer: "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/800px-Flag_of_Brazil.svg.png"
+                    question: "which flag in italy?",
+                    choices: ["https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/800px-Flag_of_Italy.svg.png", "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53E6/2F6A/5EBE/3F5A/0752/0A28/100B/04C1/Flag_of_Uruguay.png"],
+                    answer: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/800px-Flag_of_Italy.svg.png"
                 }
 
 
@@ -54,11 +50,11 @@ export default class Question extends Component {
     }
 
 
-
     generateQuestion = () => {
 
 
 
+        console.log(this.state.quiz)
         let quiz = this.state.quiz;
         let index = this.state.currentQuestionIndex;
         let currentQuestion = quiz[index].question;
@@ -81,7 +77,7 @@ export default class Question extends Component {
         let btn1 = (
             <button className="btn gradient-button gradient-button-3" value={currentChoice1} onClick={this.validateAnswer}>
 
-                <img src={currentChoice1} className="img-fluid rounded-start imgStyle" alt="..." />
+                <img src={currentChoice1} className="img-fluid rounded-start" alt="..." />
             </button>
 
         );
@@ -89,14 +85,15 @@ export default class Question extends Component {
         let btn2 = (
             <button className="btn gradient-button gradient-button-3" value={currentChoice2} onClick={this.validateAnswer}>
 
-                <img src={currentChoice2} className="img-fluid rounded-start imgStyle" alt="..." />
+                <img src={currentChoice2} className="img-fluid rounded-start" alt="..." />
             </button>
         );
 
         return (
             <div className="question" id="myDIV">
-                <div className="currentQuestion" id="questioncolor" onclick="myFunction()">
-                    {currentQuestion}</div>
+                <div className="currentQuestion" id="questioncolor" onClick="myFunction()">
+                    {currentQuestion}
+                </div>
 
                 {btn1}
 
@@ -115,16 +112,17 @@ export default class Question extends Component {
     validateAnswer = (e) => {
 
         let index = this.state.currentQuestionIndex;
-        let value = 10;
-        if (e.target.value === this.state.quiz[index].answer) {
+        // let value = 10;
+        // console.log(e.target.src)
+        if (e.target.src === this.state.quiz[index].answer) {
             document.getElementsByTagName("body")[0].style.color = "green";
             setTimeout(() => {
                 document.getElementsByTagName("body")[0].style.color = "black";
                 this.setState({
 
-                    score: this.state.score + value,
+                    score: this.state.score + 10,
                     currentQuestionIndex:
-                        (this.state.currentQuestionIndex + value) % this.state.quiz.length
+                        (this.state.currentQuestionIndex + 10) % this.state.quiz.length
                 });
 
             }, 1000);
@@ -138,10 +136,10 @@ export default class Question extends Component {
                 document.getElementsByTagName("body")[0].style.color = "black";
                 this.setState({
 
-                    score: this.state.score - value,
+                    score: this.state.score - 10,
 
                     currentQuestionIndex:
-                        (this.state.currentQuestionIndex + value) % this.state.quiz.length
+                        (this.state.currentQuestionIndex + 10) % this.state.quiz.length
 
                 });
 
